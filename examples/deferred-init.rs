11f473946a8a0a4f5d5a257a2a94fffcef880d72
Keegan McAllister <mcallister.keegan@gmail.com>
2015-05-16 16:46:24 -0700

    Support deferred init

---
+++ examples/deferred-init.rs
@@ -0,0 +1,44 @@
+// Copyright 2015 Keegan McAllister.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// See `LICENSE` in this repository.
+
+// Fuzz me with AFL_DEFER_FORKSRV=1 for a huge speedup.
+
+#![feature(plugin)]
+
+#![plugin(afl_coverage_plugin)]
+
+extern crate afl_coverage;
+
+use std::io::{self, Read};
+use std::thread;
+
+fn main() {
+    println!("An eternity in...");
+    thread::sleep_ms(500);
+
+    unsafe {
+        afl_coverage::init();
+    }
+
+    println!("the blink of an eye.");
+
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input).unwrap();
+
+    if input.starts_with("x") {
+        println!("going...");
+        if input.starts_with("xy") {
+            println!("going...");
+            if input.starts_with("xyz") {
+                println!("gone!");
+                unsafe {
+                    let x: *mut usize = 0 as *mut usize;
+                    *x = 0xBEEF;
+                }
+            }
+        }
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -13,7 +13,9 @@ use libc::exit;
 
 #[cfg(not(test))]
 #[link(name="afl-llvm-rt", kind="static")]
-extern "C" { }
+extern "C" {
+    fn __afl_manual_init();
+}
 
 #[no_mangle]
 pub unsafe extern "C" fn __afl_rs_init() {
@@ -28,4 +30,20 @@ pub unsafe extern "C" fn __afl_rs_init() {
             exit(1);
         }
     }
+}
+
+#[cfg(not(test))]
+/// Initialize the afl runtime.
+///
+/// Only needed when the env var `AFL_DEFER_FORKSRV` is set.
+///
+/// It's undefined behavior to call this function from multiple
+/// threads. You almost certainly need to call it before any
+/// additional threads are created.
+///
+/// However, calling this more than once in a single-threaded
+/// program, or calling it when not running with
+/// `AFL_DEFER_FORKSRV` is safe and a no-op.
+pub unsafe fn init() {
+    __afl_manual_init();
 }
