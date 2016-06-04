db73132a6c8ef45a30d823dfa7eca36da9b765da
Corey Farwell <coreyf@rwell.org>
2016-06-04 11:41:23 -0400

    LLVM apt is broken, switch to Docker for CI for now.

---
+++ ci/run.sh
@@ -0,0 +1,14 @@
+#!/bin/sh -e
+
+export LLVM_CONFIG=$(which llvm-config-3.8)
+
+cd afl-plugin
+cargo build --verbose
+cd ..
+cargo build --verbose
+cargo build --example hello
+cargo build --example deferred-init
+cargo build --example integer-overflow
+cargo build --example panic
+cd afl-sys
+cargo build
