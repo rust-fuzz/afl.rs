06672e615db942f8fa570aa36c5e9b26f71bbc73
Corey Farwell <coreyf@rwell.org>
2016-09-11 09:55:43 -0400

    Merge in 2.03b changes into afl-llvm-pass.so.cc.

--- afl-plugin/afl-llvm-pass.so.cc
+++ afl-plugin/afl-llvm-pass.so.cc
@@ -10,7 +10,7 @@
    from afl-as.c are Michal's fault. Keegan made some changes for Rust
    integration.
 
-   Copyright 2015 Google Inc. All rights reserved.
+   Copyright 2015, 2016 Google Inc. All rights reserved.
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
@@ -63,8 +63,7 @@ bool AFLCoverage::runOnModule(Module &M) {
   LLVMContext &C = M.getContext();
 
   IntegerType *Int8Ty  = IntegerType::getInt8Ty(C);
-  IntegerType *Int16Ty = IntegerType::getInt16Ty(C);
-  IntegerType *Int64Ty = IntegerType::getInt64Ty(C);
+  IntegerType *Int32Ty = IntegerType::getInt32Ty(C);
 
   /* Show a banner */
 
@@ -78,8 +77,8 @@ bool AFLCoverage::runOnModule(Module &M) {
 
   /* Decide instrumentation ratio */
 
-  char*    inst_ratio_str = getenv("AFL_INST_RATIO");
-  unsigned inst_ratio     = 100;
+  char* inst_ratio_str = getenv("AFL_INST_RATIO");
+  unsigned int inst_ratio = 100;
 
   if (inst_ratio_str) {
 
@@ -90,14 +89,16 @@ bool AFLCoverage::runOnModule(Module &M) {
     }
   }
 
-  /* Get globals for the SHM region and the previous location. */
+ /* Get globals for the SHM region and the previous location. Note that
+     __afl_prev_loc is thread-local. */
 
   GlobalVariable *AFLMapPtr =
       new GlobalVariable(M, PointerType::get(Int8Ty, 0), false,
                          GlobalValue::ExternalLinkage, 0, "__afl_area_ptr");
 
   GlobalVariable *AFLPrevLoc = new GlobalVariable(
-      M, Int16Ty, false, GlobalValue::ExternalLinkage, 0, "__afl_prev_loc");
+      M, Int32Ty, false, GlobalValue::ExternalLinkage, 0, "__afl_prev_loc",
+      0, GlobalValue::GeneralDynamicTLSModel, 0, false);
 
   /* Instrument all the things! */
 
@@ -114,13 +115,14 @@ bool AFLCoverage::runOnModule(Module &M) {
       /* Make up cur_loc */
 
       unsigned int cur_loc = random() % MAP_SIZE;
-      ConstantInt *CurLoc = ConstantInt::get(Int64Ty, cur_loc);
+
+      ConstantInt *CurLoc = ConstantInt::get(Int32Ty, cur_loc);
 
       /* Load prev_loc */
 
       LoadInst *PrevLoc = IRB.CreateLoad(AFLPrevLoc);
       PrevLoc->setMetadata(M.getMDKindID("nosanitize"), MDNode::get(C, None));
-      Value *PrevLocCasted = IRB.CreateZExt(PrevLoc, IRB.getInt64Ty());
+      Value *PrevLocCasted = IRB.CreateZExt(PrevLoc, IRB.getInt32Ty());
 
       /* Load SHM pointer */
 
@@ -140,7 +142,7 @@ bool AFLCoverage::runOnModule(Module &M) {
       /* Set prev_loc to cur_loc >> 1 */
 
       StoreInst *Store =
-          IRB.CreateStore(ConstantInt::get(Int16Ty, cur_loc >> 1), AFLPrevLoc);
+          IRB.CreateStore(ConstantInt::get(Int32Ty, cur_loc >> 1), AFLPrevLoc);
       Store->setMetadata(M.getMDKindID("nosanitize"), MDNode::get(C, None));
 
       inst_blocks++;
