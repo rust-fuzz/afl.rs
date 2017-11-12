8ea8657e2cbb94a854ed08eb47078202b2900eb6
Corey Farwell <coreyf@rwell.org>
2017-11-12 10:54:48 -0500

    Bump afl-2.34b -> afl-2.52b.
    Primarily doing this so the diff in:
    
    https://github.com/rust-fuzz/afl.rs/pull/116/
    
    appears cleaner.

--- afl-sys/build.rs
+++ afl-sys/build.rs
@@ -3,14 +3,14 @@ extern crate gcc;
 fn main() {
     // TODO: use better values here
     gcc::Config::new()
-        .file("afl-2.34b/afl-fuzz.c")
+        .file("afl-2.52b/afl-fuzz.c")
         .define("BIN_PATH", Some("\"/tmp/bin/\""))  // TODO: does this value matter?
         .define("DOC_PATH", Some("\"/tmp/bin/\""))  // TODO: does this value matter?
-        .define("VERSION", Some("\"2.34b\""))
+        .define("VERSION", Some("\"2.52b\""))
         .define("main", Some("afl_fuzz_main"))  // Rename 'main' function
         .flag("-funroll-loops")
         .flag("-w")
-        .include("afl-2.34b")
+        .include("afl-2.52b")
         .opt_level(3)
         .compile("libafl.a");
 }
