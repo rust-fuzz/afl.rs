c9537eabd412591b91e328f41451c4aba199c684
Corey Farwell <coreyf@rwell.org>
2017-11-19 21:40:49 -0500

    Introduce more helpful CLI using clap-rs.

--- src/bin/cargo-afl.rs
+++ src/bin/cargo-afl.rs
@@ -1,3 +1,5 @@
+#[macro_use]
+extern crate clap;
 extern crate rustc_version;
 extern crate xdg;
 
@@ -9,6 +11,8 @@ use std::process::{self, Command};
 mod dirs;
 
 fn main() {
+    let _ = clap_app().get_matches();
+
     let mut args = env::args().skip(2).peekable(); // skip `cargo` and `afl`
     match args.peek().map(|s| &**s) {
         Some("analyze") => run_afl(args, "afl-analyze"),
@@ -21,6 +25,101 @@ fn main() {
         Some("whatsup") => run_afl(args, "afl-whatsup"),
         _ => run_cargo(args),
     }
+}
+
+fn clap_app() -> clap::App<'static, 'static> {
+    clap::App::new("cargo afl").bin_name("cargo").subcommand(
+        clap::SubCommand::with_name("afl")
+            .version(crate_version!())
+            .setting(clap::AppSettings::ArgRequiredElseHelp)
+            .setting(clap::AppSettings::TrailingVarArg)
+            .setting(clap::AppSettings::AllowExternalSubcommands)
+            .usage("cargo afl [SUBCOMMAND or Cargo SUBCOMMAND]")
+            .after_help(
+                "In addition to the subcommands above, Cargo subcommands are also \
+                     supported (see `cargo help` for a list of all Cargo subcommands).",
+            )
+            .subcommand(
+                clap::SubCommand::with_name("analyze")
+                    .about("Invoke afl-analyze")
+                    .setting(clap::AppSettings::AllowLeadingHyphen)
+                    .setting(clap::AppSettings::DisableHelpSubcommand)
+                    .setting(clap::AppSettings::DisableVersion)
+                    .arg(clap::Arg::with_name("h").short("h").hidden(true))
+                    .arg(clap::Arg::with_name("help").long("help").help("known bug! rewrite as '-- --help' to see AFL help"))
+                    .arg(clap::Arg::with_name("afl-analyze args").multiple(true)),
+            )
+            .subcommand(
+                clap::SubCommand::with_name("cmin")
+                    .about("Invoke afl-cmin")
+                    .setting(clap::AppSettings::AllowLeadingHyphen)
+                    .setting(clap::AppSettings::DisableHelpSubcommand)
+                    .setting(clap::AppSettings::DisableVersion)
+                    .arg(clap::Arg::with_name("h").short("h").hidden(true))
+                    .arg(clap::Arg::with_name("help").long("help").help("known bug! rewrite as '-- --help' to see AFL help"))
+                    .arg(clap::Arg::with_name("afl-cmin args").multiple(true)),
+            )
+            .subcommand(
+                clap::SubCommand::with_name("fuzz")
+                    .about("Invoke afl-fuzz")
+                    .setting(clap::AppSettings::AllowLeadingHyphen)
+                    .setting(clap::AppSettings::DisableHelpSubcommand)
+                    .setting(clap::AppSettings::DisableVersion)
+                    .arg(clap::Arg::with_name("h").short("h").hidden(true))
+                    .arg(clap::Arg::with_name("help").long("help").help("known bug! rewrite as '-- --help' to see AFL help"))
+                    .arg(clap::Arg::with_name("afl-fuzz args").multiple(true)),
+            )
+            .subcommand(
+                clap::SubCommand::with_name("gotcpu")
+                    .about("Invoke afl-gotcpu")
+                    .setting(clap::AppSettings::AllowLeadingHyphen)
+                    .setting(clap::AppSettings::DisableHelpSubcommand)
+                    .setting(clap::AppSettings::DisableVersion)
+                    .arg(clap::Arg::with_name("h").short("h").hidden(true))
+                    .arg(clap::Arg::with_name("help").long("help").help("known bug! rewrite as '-- --help' to see AFL help"))
+                    .arg(clap::Arg::with_name("afl-gotcpu args").multiple(true)),
+            )
+            .subcommand(
+                clap::SubCommand::with_name("plot")
+                    .about("Invoke afl-plot")
+                    .setting(clap::AppSettings::AllowLeadingHyphen)
+                    .setting(clap::AppSettings::DisableHelpSubcommand)
+                    .setting(clap::AppSettings::DisableVersion)
+                    .arg(clap::Arg::with_name("h").short("h").hidden(true))
+                    .arg(clap::Arg::with_name("help").long("help").help("known bug! rewrite as '-- --help' to see AFL help"))
+                    .arg(clap::Arg::with_name("afl-plot args").multiple(true)),
+            )
+            .subcommand(
+                clap::SubCommand::with_name("showmap")
+                    .about("Invoke afl-showmap")
+                    .setting(clap::AppSettings::AllowLeadingHyphen)
+                    .setting(clap::AppSettings::DisableHelpSubcommand)
+                    .setting(clap::AppSettings::DisableVersion)
+                    .arg(clap::Arg::with_name("h").short("h").hidden(true))
+                    .arg(clap::Arg::with_name("help").long("help").help("known bug! rewrite as '-- --help' to see AFL help"))
+                    .arg(clap::Arg::with_name("afl-showmap args").multiple(true)),
+            )
+            .subcommand(
+                clap::SubCommand::with_name("tmin")
+                    .about("Invoke afl-tmin")
+                    .setting(clap::AppSettings::AllowLeadingHyphen)
+                    .setting(clap::AppSettings::DisableHelpSubcommand)
+                    .setting(clap::AppSettings::DisableVersion)
+                    .arg(clap::Arg::with_name("h").short("h").hidden(true))
+                    .arg(clap::Arg::with_name("help").long("help").help("known bug! rewrite as '-- --help' to see AFL help"))
+                    .arg(clap::Arg::with_name("afl-tmin args").multiple(true)),
+            )
+            .subcommand(
+                clap::SubCommand::with_name("whatsup")
+                    .about("Invoke afl-whatsup")
+                    .setting(clap::AppSettings::AllowLeadingHyphen)
+                    .setting(clap::AppSettings::DisableHelpSubcommand)
+                    .setting(clap::AppSettings::DisableVersion)
+                    .arg(clap::Arg::with_name("h").short("h").hidden(true))
+                    .arg(clap::Arg::with_name("help").long("help").help("known bug! rewrite as '-- --help' to see AFL help"))
+                    .arg(clap::Arg::with_name("afl-whatsup args").multiple(true)),
+            ),
+    )
 }
 
 fn run_afl<I, S>(args: I, cmd: &str)
