3b113857595aea621392573603f4800d4396661a
Pedro Côrte-Real <pedro@pedrocr.net>
2019-02-22 14:13:04 +0000

    Fix passing extra RUSTFLAGS
    When the fix for #141 is in effect if you pass extra RUSTFLAGS there is
    no space separating them and the command line breaks. Fix that by adding
    a space at the end of those flags, just like it's done in the common
    string.

--- src/bin/cargo-afl.rs
+++ src/bin/cargo-afl.rs
@@ -194,8 +194,8 @@ where
         // work around https://github.com/rust-fuzz/afl.rs/issues/141 /
         // https://github.com/rust-lang/rust/issues/53945, can be removed once
         // those are fixed.
-        rustflags.push_str("-Clink-arg=-fuse-ld=gold");
-        rustdocflags.push_str("-Clink-arg=-fuse-ld=gold");
+        rustflags.push_str("-Clink-arg=-fuse-ld=gold ");
+        rustdocflags.push_str("-Clink-arg=-fuse-ld=gold ");
     }
 
     // add user provided flags
