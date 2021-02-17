e514cd39dfd3b53ba6d887ba71b653ec7b078d1d
Andrea Fioraldi <andreafioraldi@gmail.com>
2021-02-17 23:57:13 +0100

    update to AFL++ 3.01a && enable persistent shared memory fuzzing

--- build.rs
+++ build.rs
@@ -25,9 +25,6 @@ fn build_afl(out_dir: &Path) {
     command
         .current_dir(AFL_SRC_PATH)
         .args(&["clean", "all", "install"])
-        // Rely on LLVM’s built-in execution tracing feature instead of AFL’s
-        // LLVM passi instrumentation.
-        .env("AFL_TRACE_PC", "1")
         .env("DESTDIR", out_dir)
         .env("PREFIX", "");
     // sets AFL_NO_X86 to compile for ARM arch
@@ -39,15 +36,8 @@ fn build_afl(out_dir: &Path) {
 }
 
 fn build_afl_llvm_runtime() {
-    let status = Command::new("make")
-        .current_dir(Path::new(AFL_SRC_PATH).join("llvm_mode"))
-        .arg("../afl-llvm-rt.o")
-        .status()
-        .expect("could not compile 'afl-llvm-rt.o'");
-    assert!(status.success());
-
     std::fs::copy(
-        Path::new(&AFL_SRC_PATH).join("afl-llvm-rt.o"),
+        Path::new(&AFL_SRC_PATH).join("afl-compiler-rt.o"),
         common::object_file_path(),
     )
     .expect("Couldn't copy object file");
