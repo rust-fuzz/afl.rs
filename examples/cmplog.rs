bd8e165808ad4117572a5bf741499e61e95d9530
Louis <louis@srlabs.de>
2023-05-04 17:44:33 +0200

    Add CmpLog example

---
+++ examples/cmplog.rs
@@ -0,0 +1,31 @@
+fn main() {
+    // This fuzz harness demonstrates the capabilities of CmpLog.
+    // Simply run the fuzzer with the `-c 0` flag and it should find the crash immediately.
+    afl::fuzz!(|data: &[u8]| {
+        if data.len() != 16 {
+            return;
+        }
+        if data[0] != b'A' {
+            return;
+        }
+        if data[1] != b'B' {
+            return;
+        }
+        if data[2] != b'C' {
+            return;
+        }
+        if data[3] != b'D' {
+            return;
+        }
+
+        if data[4..8] != 0x69694141_i32.to_le_bytes() {
+            return;
+        };
+
+        if data[8..12] != *b"1234" || data[12..16] != *b"EFGH" {
+            return;
+        };
+
+        panic!("BOOM");
+    });
+}
