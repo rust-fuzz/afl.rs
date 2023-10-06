2eca9846b7965c8f3033ace2289f1143368a97ea
Lukasz Rubaszewski <lukasz.rubaszewski@rdx.works>
2023-10-06 18:41:19 +0200

    Fix running AFL with no 'fuzzing' flag

--- afl/build.rs
+++ afl/build.rs
@@ -8,10 +8,12 @@ fn main() {
         || env::var("TESTING_BUILD").is_ok();
 
     let cfg_fuzzing = env::var("CARGO_CFG_FUZZING").is_ok();
+    let no_cfg_fuzzing = !env::var("AFL_NO_CFG_FUZZING").unwrap_or_default().is_empty();
 
-    let feature_no_fuzzing = env::var("CARGO_FEATURE_NO_FUZZING").is_ok();
+    // afl-fuzz is sensitive to AFL_ env variables. Let's remove this particular one - it did it's job
+    env::remove_var("AFL_NO_CFG_FUZZING");
 
-    if building_in_cargo_home && !cfg_fuzzing && !feature_no_fuzzing {
+    if building_in_cargo_home && !cfg_fuzzing && !no_cfg_fuzzing {
         println!("cargo:warning=You appear to be building `afl` not under `cargo-afl`.");
         println!("cargo:warning=Perhaps you used `cargo build` instead of `cargo afl build`?");
     }
