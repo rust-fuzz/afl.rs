ead58d0422b1b16615627bb3620c1ee50fed92d9
Samuel Moelius <sam@moeli.us>
2024-07-14 16:52:17 -0400

    Use `anyhow`

--- cargo-afl/src/common.rs
+++ cargo-afl/src/common.rs
@@ -1,8 +1,8 @@
 #![deny(clippy::disallowed_macros, clippy::expect_used, clippy::unwrap_used)]
 
+use anyhow::{Context, Result};
 use std::env;
 use std::ffi::OsStr;
-use std::io::{Error, Result};
 use std::path::{Path, PathBuf};
 
 fn xdg_dir() -> Result<xdg::BaseDirectories> {
@@ -10,7 +10,7 @@ fn xdg_dir() -> Result<xdg::BaseDirectories> {
     let prefix = Path::new("afl.rs")
         .join(afl_rustc_version)
         .join(pkg_version());
-    xdg::BaseDirectories::with_prefix(prefix).map_err(Error::other)
+    xdg::BaseDirectories::with_prefix(prefix).map_err(Into::into)
 }
 
 fn data_dir(dir_name: &str) -> Result<PathBuf> {
@@ -23,13 +23,13 @@ fn data_dir(dir_name: &str) -> Result<PathBuf> {
     // don't change that for now for normal builds.
     // smoelius: AFL++ is no longer built on docs.rs.
     let xdg_dir = xdg_dir()?;
-    xdg_dir.create_data_directory(dir_name)
+    xdg_dir.create_data_directory(dir_name).map_err(Into::into)
 }
 
 const SHORT_COMMIT_HASH_LEN: usize = 7;
 
 pub fn afl_rustc_version() -> Result<String> {
-    let version_meta = rustc_version::version_meta().map_err(Error::other)?;
+    let version_meta = rustc_version::version_meta()?;
     let mut ret = String::from("rustc-");
     ret.push_str(&version_meta.semver.to_string());
     if let Some(commit_hash) = version_meta.commit_hash {
@@ -73,8 +73,12 @@ pub fn archive_file_path() -> Result<PathBuf> {
 #[allow(dead_code)]
 pub fn plugins_available() -> Result<bool> {
     let afl_llvm_dir = afl_llvm_dir()?;
-    for result in afl_llvm_dir.read_dir()? {
-        let entry = result?;
+    for result in afl_llvm_dir
+        .read_dir()
+        .with_context(|| format!("could not read {afl_llvm_dir:?}"))?
+    {
+        let entry =
+            result.with_context(|| format!("could not read `DirEntry` in {afl_llvm_dir:?}"))?;
         let file_name = entry.file_name();
         if Path::new(&file_name).extension() == Some(OsStr::new("so")) {
             return Ok(true);
