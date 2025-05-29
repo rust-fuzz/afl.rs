ee1d39bf2de4e15a187ec24ef5919e7da8747087
Samuel Moelius <sam@moeli.us>
2025-05-29 04:42:56 -0400

    Update publish.sh

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,13 +3,8 @@
 # set -x
 set -euo pipefail
 
-if [[ $# -ne 0 ]]; then
-    echo "$0: expect no arguments" >&2
-    exit 1
-fi
-
 for X in afl cargo-afl; do
     pushd "$X"
-    cargo publish
+    cargo publish "$@"
     popd
 done
