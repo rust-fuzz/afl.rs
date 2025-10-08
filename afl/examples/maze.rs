0e10bf62bee6adc2a06e0a81e9d0bdb084e64c2f
van Hauser <vh@thc.org>
2025-10-08 15:20:44 +0200

    Update afl/examples/maze.rs
    Co-authored-by: Samuel Moelius <35515885+smoelius@users.noreply.github.com>

--- afl/examples/maze.rs
+++ afl/examples/maze.rs
@@ -40,13 +40,7 @@ cddbddcccacaaaccaaaaaabbbbbbbbbbaaccccccccccccaaccaabbbbbbbbbbbbbbbbbbbbbbbbbaac
 #![allow(clippy::cast_possible_truncation)]
 
 #[cfg(fuzzing)]
-use afl::ijon_hashint;
-#[cfg(fuzzing)]
-use afl::ijon_hashstr;
-#[cfg(fuzzing)]
-use afl::ijon_set;
-#[cfg(fuzzing)]
-use std::ffi::CString;
+use afl::{ijon_hashint, ijon_set};
 
 fn main() {
     afl::fuzz!(|data: &[u8]| {
